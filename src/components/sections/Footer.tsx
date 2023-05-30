92ce4c17421604f652de63a7f1404579f78f787a
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-05-30 12:46:57 +0700

    fix footer icon animation

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -11,22 +11,28 @@ export default function Footer() {
           <a
             href="mailto:mdanangpriambodo@gmail.com"
             target="_blank"
-            className="font-roboto-mono text-[14px] tracking-widest text-white font-light sm:fixed sm:bottom-[15rem] sm:-right-20 sm:rotate-90 hover:text-teal hover:-translate-y-2 duration-500 transition peer"
+            className="font-roboto-mono text-[14px] tracking-widest text-white font-light sm:fixed sm:bottom-[15rem] sm:-right-[5.3rem]  group peer"
           >
-            mdanangpriambodo@gmail.com
+            <div className="w-64 group-hover:text-teal group-hover:-translate-y-2 duration-500 transition sm:rotate-90">mdanangpriambodo@gmail.com</div>
           </a>
           <div className="border-r-[3px] h-36 border-r-gray sm:fixed sm:-bottom-12 sm:right-12 peer-hover:-translate-y-2 duration-500"></div>
         </div>
         <div>
           <div className="flex justify-center gap-6 gap-y-8 md:flex-col md:fixed md:bottom-32 md:left-6">
             <a href="https://www.linkedin.com/in/mdanangp/" target="_blank">
-              <Linkedin className="fill-white hover:fill-teal w-6 h-6 hover:-translate-y-2 duration-300 transition" />
+              <div className="h-6 group">
+                <Linkedin className="fill-white group-hover:fill-teal w-6 h-6 group-hover:-translate-y-2 duration-300 transition" />
+              </div>
             </a>
             <a href="https://codepen.io/mcdanang" target="_blank">
-              <Codepen className="stroke-white hover:stroke-teal w-6 h-6 hover:-translate-y-2 duration-300 transition" />
+              <div className="h-6 group">
+                <Codepen className="stroke-white group-hover:stroke-teal w-6 h-6 group-hover:-translate-y-2 duration-300 transition" />
+              </div>
             </a>
             <a href="https://github.com/mcdanang/" target="_blank">
-              <Github className="stroke-white hover:stroke-teal w-6 h-6 hover:-translate-y-2 duration-300 transition" />
+              <div className="h-6 group">
+                <Github className="stroke-white group-hover:stroke-teal w-6 h-6 group-hover:-translate-y-2 duration-300 transition" />
+              </div>
             </a>
           </div>
           <div className="hidden md:block border-r-[3px] h-36 border-r-gray sm:fixed sm:-bottom-12 sm:left-9 peer-hover:-translate-y-2 duration-300"></div>
