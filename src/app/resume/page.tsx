c11f369b2bc5ee2a9751af52c38e4f0326801f7b
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2025-05-07 23:10:14 +0700

    update cv

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,7 @@
 export default function MyResume() {
 	return (
 		<div>
-			<iframe src="/pdfs/Muhamad_Danang_Priambodo_CV.pdf" className="w-screen h-screen" />
+			<iframe src="/pdfs/Muhamad_Danang_Priambodo_Resume.pdf" className="w-screen h-screen" />
 		</div>
 	);
 }
